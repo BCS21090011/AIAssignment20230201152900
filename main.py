5cd5cf444f31d68c3d393d673166e9592246fb70
BCS21090011 <bcs21090011@student.bcs.edu.my>
2023-01-02 16:57:46 +0800

    Added some comments.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
+# Read the file:
 oriDataset: pd.DataFrame = pd.read_csv("E:\School Stuffs\Bachelor in Computer Science\Year 2\Sem 1\Artificial intelligence\Group assignment\Species_clustering_classification.csv")
 
-healthDict: dict = oriDataset[["health", "health_number"]].set_index("health").to_dict()["health_number"]
-speciesDict: dict = oriDataset[["species", "species_number"]].set_index("species").to_dict()["species_number"]
+healthDict: dict = oriDataset[["health", "health_number"]].set_index("health").to_dict()["health_number"]   # Contain the values in "health" as key and it's respective "health_number" as value.
+speciesDict: dict = oriDataset[["species", "species_number"]].set_index("species").to_dict()["species_number"]  # Contain the values in "species" as key and it's respective "species_number" as value.
 
-dataset: pd.DataFrame = oriDataset.copy()
+dataset: pd.DataFrame = oriDataset.copy()   # Will use this dataset. oriDataset won't be used or affected.
 dataset.drop(["health", "species"], axis=1, inplace=True)
 
 inputCols: list[str] = ["Area", "species_number", "height", "crown_width", "dbh"]
@@ -18,12 +19,13 @@ y: pd.Series = dataset[outputCols]
 dTree: DecisionTreeClassifier = DecisionTreeClassifier()
 dTree = dTree.fit(X.values, y.values)
 
-# Predict will only take 2d list.
+# Make prediction:
 def Predict(inputVals: list)->int:
     vals: list[list] = [inputVals]
     predResult: int = dTree.predict(vals)[0]
     return predResult
 
+# Get the key of value in dictionary:
 def GetKeyVal(dictionary: dict, val: float)->str:
     if val in dictionary.values():
         key: str = [k for k,v in dictionary.items() if v == val]
@@ -31,6 +33,7 @@ def GetKeyVal(dictionary: dict, val: float)->str:
     else:
         return None
 
+# Get the value of key in dictionary:
 def GetValKey(dictionary: dict, key: str)->float:
     if key in dictionary.keys():
         val: float = dictionary[key]
